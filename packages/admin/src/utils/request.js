18d2b9a1a126aa89b22d855ce51472851217d28c
dreamming <527386108@qq.com>
2023-01-30 15:47:46 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ packages/admin/src/utils/request.js
@@ -0,0 +1,58 @@
+import i18n from 'i18next';
+
+export default async function request(url, opts = {}) {
+  if (typeof url === 'object') {
+    opts = url;
+  } else if (typeof url === 'string') {
+    opts.url = url;
+  }
+
+  if (!opts.headers) {
+    opts.headers = {};
+  }
+  if (opts.body && !(opts.body instanceof FormData)) {
+    opts.headers['Content-Type'] = 'application/json';
+    opts.body = JSON.stringify(opts.body);
+  }
+
+  let token = window.TOKEN || sessionStorage.getItem('TOKEN');
+
+  if (!token) {
+    token = localStorage.getItem('TOKEN');
+  }
+  if (token) {
+    opts.headers.Authorization = `Bearer ${token}`;
+  }
+
+  let baseUrl = window.serverURL;
+
+  if (!baseUrl) {
+    const match = location.pathname.match(/(.*?\/)ui/);
+
+    baseUrl = match ? match[1] : '/';
+  }
+
+  const joiner = opts.url.includes('?') ? '&' : '?';
+
+  return fetch(`${baseUrl}${opts.url}${joiner}lang=${i18n.language}`, opts)
+    .then((resp) => {
+      if (resp.ok) {
+        const __version = resp.headers.get('x-waline-version');
+
+        return resp.json().then((resp) => ({ __version, ...resp }));
+      }
+
+      if (resp.status === 401) {
+        throw new Error(401);
+      }
+
+      throw new Error(`${resp.status}: ${resp.statusText}`);
+    })
+    .then((resp) => {
+      if (resp.errno !== 0) {
+        throw new Error(resp.errmsg);
+      }
+
+      return { __version: resp.__version, ...resp.data };
+    });
+}
